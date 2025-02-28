c19fa78beb1c7880ad8d4220d0e5a68c444bdd02
RABTANK <1300866375@qq.com>
2025-02-28 15:18:38 +0800

    a

--- classes/commandHandler.py
+++ classes/commandHandler.py
@@ -1,7 +1,7 @@
 from auth.static import Static
 import requests
 class CommandHandler:
-    def __init__(self, raw, user_union_id,pre_message_id):
+    def __init__(self, raw, user_union_id,pre_message_id,messgae_type,group_id):
         """初始化命令处理器实例。
         
         Args:
@@ -14,6 +14,8 @@ class CommandHandler:
             2. 拆分命令和参数
             3. 存储用户唯一ID和上条消息ID
         """
+        self.messgae_type = messgae_type
+        self.group_id = group_id
         self.raw = raw.strip()
         parts = self.raw.split(maxsplit=1)
         self.command = parts[0][1:]  # 去掉斜杠
@@ -35,6 +37,12 @@ class CommandHandler:
 
     def execute_command(self):
         commands = [self.command] + self.args
-        response = requests.get(Static.PLUGS_URL,params={'command': commands}  )# requests会自动处理数组参数
+        response = requests.get(
+            Static.PLUGS_URL,
+            params={
+                'command': commands,
+                'userUnionId': self.user_union_id  # 新增用户唯一标识参数
+            }
+        )
         print(f"响应内容：{response.text}")
         return 'ok'
